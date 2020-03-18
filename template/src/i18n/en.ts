4a502811aeaca39ff5d0baa3d4f53eee50111a52
Emile Silas SARE <emile.silas@gmail.com>
2020-03-18 11:53:02 +0100

    updated

--- template/src/i18n/en.ts
+++ template/src/i18n/en.ts
@@ -1,3 +1,6 @@
-import { tI18nDefinition } from "oweb";
+import { tI18nDefinition } from 'oweb';
 
-export default {} as tI18nDefinition;
+export default {
+	UX_LANG_YOU_ARE_OFFLINE:
+		'You are not connected. Please check your internet connection.',
+} as tI18nDefinition;
--- template/src/i18n/fr.ts
+++ template/src/i18n/fr.ts
@@ -1,3 +1,6 @@
-import { tI18nDefinition } from "oweb";
+import { tI18nDefinition } from 'oweb';
 
-export default {} as tI18nDefinition;
+export default {
+	UX_LANG_YOU_ARE_OFFLINE:
+		"Vous n'êtes pas connecté. Veuillez, vérifiez votre connexion internet.",
+} as tI18nDefinition;
